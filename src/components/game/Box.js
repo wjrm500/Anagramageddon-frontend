57e8cbe3c08ec9dfecf3bbebaa260f008192d7fe
William May <wjrm500@gmail.com>
2023-01-15 00:55:38 +0800

    Darkening background of volatile boxes so they stand out a bit more

--- src/components/game/Box.js
+++ src/components/game/Box.js
@@ -29,12 +29,21 @@ class Box extends React.Component {
     const innerBoxClasses = ["innerBox"]
     if (!this.state.active) innerBoxClasses.push("inactive")
     if (this.state.box.volatile) innerBoxClasses.push("shaking")
+    const standardBackgroundColor = "rgb(238, 238, 238)"
+    let backgroundColor = this.state.box.player ? this.getColor() : standardBackgroundColor
+    if (this.state.box.volatile) {
+      const colorValues = backgroundColor.match(/\d+/g);
+      const colorValuesAsNumbers = colorValues.map(value => parseInt(value));
+      const multiplier = 0.8 // Between 0 and 1 - higher means volatile boxes will have a lighter colour relative to normal boxes
+      const newColor = "rgb(" + (colorValuesAsNumbers[0] * multiplier) + "," + (colorValuesAsNumbers[1] * multiplier) + "," + (colorValuesAsNumbers[2] * multiplier) + ")"
+      backgroundColor = newColor
+    }
     return (
       <div className="outerBox">
         <div
           className={innerBoxClasses.join(" ")}
           style={{
-            backgroundColor: this.state.box.player ? this.getColor() : "",
+            backgroundColor: backgroundColor,
             color: this.state.box.player ? "white" : ""
           }}
           onClick={this.state.onClick}
