63806a54f72a27d0143ae63e183e5feb0a3c9c41
William May <wjrm500@gmail.com>
2023-01-15 16:33:48 +0800

    Ensuring hovering a box changes its colour and works in sync with colour changes from player ownership and volatility

--- src/components/game/Box.js
+++ src/components/game/Box.js
@@ -6,7 +6,8 @@ class Box extends React.Component {
     this.state = {
       box: props.box,
       active: props.active,
-      onClick: props.onClick
+      onClick: props.onClick,
+      hovered: false
     }
     this.getColor = this.getColor.bind(this)
   }
@@ -25,28 +26,46 @@ class Box extends React.Component {
     return `rgba(${colorTuple[0]}, ${colorTuple[1]}, ${colorTuple[2]}, ${alpha})`
   }
 
+  darkenColor(colorString, factor) { // Factor between 0 and 1 - a lower number gives a darker colour
+    const colorValues = colorString.match(/\d+/g)
+    const colorValuesAsNumbers = colorValues.map(value => parseInt(value))
+    const newColor = `rgb(${colorValuesAsNumbers.map(x => parseInt(x * factor)).join(', ')})`
+    return newColor
+  }
+
   render() {
     const innerBoxClasses = ["innerBox"]
     if (!this.state.active) innerBoxClasses.push("inactive")
     if (this.state.box.volatile) innerBoxClasses.push("shaking")
     const standardBackgroundColor = "rgb(238, 238, 238)"
     let backgroundColor = this.state.box.player ? this.getColor() : standardBackgroundColor
-    if (this.state.box.volatile) {
-      const colorValues = backgroundColor.match(/\d+/g);
-      const colorValuesAsNumbers = colorValues.map(value => parseInt(value));
-      const multiplier = 0.8 // Between 0 and 1 - higher means volatile boxes will have a lighter colour relative to normal boxes
-      const newColor = "rgb(" + (colorValuesAsNumbers[0] * multiplier) + "," + (colorValuesAsNumbers[1] * multiplier) + "," + (colorValuesAsNumbers[2] * multiplier) + ")"
-      backgroundColor = newColor
+    let cursor = "auto"
+    if (!this.state.box.player && this.state.box.volatile) {
+      backgroundColor = this.darkenColor(backgroundColor, 0.8)
     }
+    if (!this.state.box.player && this.state.active && this.state.hovered) {
+      backgroundColor = this.darkenColor(backgroundColor, 0.9)
+      cursor = "pointer"
+    }
+    console.log(backgroundColor)
     return (
       <div className="outerBox">
         <div
           className={innerBoxClasses.join(" ")}
           style={{
             backgroundColor: backgroundColor,
-            color: this.state.box.player ? "white" : ""
+            color: this.state.box.player ? "white" : "",
+            cursor: cursor
           }}
           onClick={this.state.onClick}
+          onMouseEnter={() => {
+            this.state.hovered = true
+            this.forceUpdate()
+          }}
+          onMouseLeave={() => {
+            this.state.hovered = false
+            this.forceUpdate()
+          }}
           >
           {this.state.box.letter}
         </div>
