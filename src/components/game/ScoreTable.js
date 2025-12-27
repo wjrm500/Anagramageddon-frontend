7f2fcee153f9c3ec1f58803472df03c77c3e3963
William May <wjrm500@gmail.com>
2025-12-27 00:09:05 +0000

    Show score needed to win above score table

--- src/components/game/ScoreTable.js
+++ src/components/game/ScoreTable.js
@@ -9,6 +9,7 @@ const truncateName = (name, maxLength = 10) => {
 const ScoreTable = () => {
   const playerCollection = useSelector(state => state.playerCollection)
   const playerIndex = useSelector(state => state.playerIndex)
+  const winningScore = useSelector(state => state.winningScore)
   const players = playerCollection.getPlayers()
   const playerCount = players.length
 
@@ -33,25 +34,31 @@ const ScoreTable = () => {
   // 2 players: single row, player 0 left, player 1 right
   if (playerCount === 2) {
     return (
-      <div id="scoreTable" className="players-2">
-        {players.map((player, idx) => renderCell(player, idx))}
-      </div>
+      <>
+        <div className="winning-score-info">First to {winningScore} wins</div>
+        <div id="scoreTable" className="players-2">
+          {players.map((player, idx) => renderCell(player, idx))}
+        </div>
+      </>
     )
   }
 
   // 3-4 players: 2x2 grid, clockwise from top-left
   // Cell order: [TL, TR, BL, BR] → player order: [0, 1, 3, 2]
   const cellToPlayer = [0, 1, 3, 2]
-  
+
   return (
-    <div id="scoreTable" className={`players-${playerCount}`}>
-      {cellToPlayer.map((playerIdx, cellIndex) => {
-        const player = players[playerIdx]
-        return player 
-          ? renderCell(player, playerIdx)
-          : <div key={`empty-${cellIndex}`} className="score-cell empty" />
-      })}
-    </div>
+    <>
+      <div className="winning-score-info">First to {winningScore} wins</div>
+      <div id="scoreTable" className={`players-${playerCount}`}>
+        {cellToPlayer.map((playerIdx, cellIndex) => {
+          const player = players[playerIdx]
+          return player
+            ? renderCell(player, playerIdx)
+            : <div key={`empty-${cellIndex}`} className="score-cell empty" />
+        })}
+      </div>
+    </>
   )
 }
 
